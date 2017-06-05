42b10a76f68f56018a65fe3bfca04f008ce276c0
Florian Friedrich <ffried@me.com>
2017-06-05 16:25:19 +0200

    Remove trailing whitespaces

--- Sources/Version.swift
+++ Sources/Version.swift
@@ -28,15 +28,15 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
             assert(newValue.first { $0.isEmpty } == nil)
         }
     }
-    
+
     public var hashValue: Int {
         return major.hashValue ^ minor.hashValue ^ patch.hashValue ^ prerelease.hashValue
     }
-    
+
     public var description: String {
         return versionString()
     }
-    
+
     public init(major: Int, minor: Int = 0, patch: Int = 0, prerelease: String = "", metadata: [String] = []) {
         assert(major >= 0)
         assert(minor >= 0)
@@ -44,22 +44,22 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
         assert(CharacterSet(charactersIn: prerelease).isSubset(of: .versionSuffixAllowed))
         assert(metadata.first { !CharacterSet(charactersIn: $0).isSubset(of: .versionSuffixAllowed) } == nil)
         assert(metadata.first { $0.isEmpty } == nil)
-        
+
         self.major = major
         self.minor = minor
         self.patch = patch
         self.prerelease = prerelease
         self.metadata = metadata
     }
-    
+
     public init(major: Int, minor: Int = 0, patch: Int = 0, prerelease: String = "", metadata: String...) {
         self.init(major: major, minor: minor, patch: patch, prerelease: prerelease, metadata: metadata)
     }
-    
+
     public init?(_ description: String) {
         guard description.range(of: "^([0-9]+\\.){2}[0-9]+(-[0-9A-Za-z-]+)?(\\+([0-9A-Za-z-]+\\.?)*)?$", options: [.regularExpression]) != nil
             else { return nil }
-        
+
         // This should be fine after above's regular expression
         let idx = description.range(of: "[0-9](\\+|-)", options: [.regularExpression]).map { description.index(before: $0.upperBound) } ?? description.endIndex
         let parts = description.substring(to: idx).components(separatedBy: ".")
@@ -68,7 +68,7 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
             let minor = Int(parts[1]),
             let patch = Int(parts[2])
             else { return nil }
-        
+
         let prerelease: String
         if let searchRange = description.range(of: "(^|\\.)[0-9]+-[0-9A-Za-z-]+(\\+|$)", options: [.regularExpression]),
             case let substr = description.substring(with: searchRange),
@@ -77,7 +77,7 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
         } else {
             prerelease = ""
         }
-        
+
         let metadata: [String]
         if let range = description.range(of: "\\+([0-9A-Za-z-]+\\.?)+$", options: [.regularExpression]) {
             let metadataString = description.substring(with: description.index(after: range.lowerBound)..<range.upperBound)
@@ -85,10 +85,10 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
         } else {
             metadata = []
         }
-        
+
         self.init(major: major, minor: minor, patch: patch, prerelease: prerelease, metadata: metadata)
     }
-    
+
     public func versionString(includingPrerelease: Bool = true, includingMetadata: Bool = true) -> String {
         var versionString = "\(major).\(minor).\(patch)"
         if includingPrerelease, !prerelease.isEmpty {
@@ -99,13 +99,13 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
         }
         return versionString
     }
-    
+
     public static func ==(lhs: Version, rhs: Version) -> Bool {
         return (lhs.major, lhs.minor, lhs.patch, lhs.prerelease)
                                     ==
                (rhs.major, rhs.minor, rhs.patch, rhs.prerelease)
     }
-    
+
     public static func <(lhs: Version, rhs: Version) -> Bool {
         return (lhs.major, lhs.minor, lhs.patch)
                                 <
@@ -113,7 +113,7 @@ public struct Version: Hashable, Comparable, LosslessStringConvertible {
                                 || // A version with a prerelease has a lower precedence than the same without
                ((!lhs.prerelease.isEmpty && rhs.prerelease.isEmpty) || (lhs.prerelease < rhs.prerelease))
     }
-    
+
     public static func >(lhs: Version, rhs: Version) -> Bool {
         return (lhs.major, lhs.minor, lhs.patch)
                                 >
