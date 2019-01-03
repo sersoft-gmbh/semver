3f35a67a8adaaa7e9088a840fa5987cdf75bfb92
Florian Friedrich <ffried@me.com>
2019-01-03 15:50:29 +0100

    Add standalone Xcode project for integration

---
+++ Xcode/SemVer/SemVer.h
@@ -0,0 +1,19 @@
+//
+//  SemVer.h
+//  SemVer
+//
+//  Created by Florian Friedrich on 03.01.19.
+//  Copyright © 2019 ser.soft GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SemVer.
+FOUNDATION_EXPORT double SemVerVersionNumber;
+
+//! Project version string for SemVer.
+FOUNDATION_EXPORT const unsigned char SemVerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SemVer/PublicHeader.h>
+
+
