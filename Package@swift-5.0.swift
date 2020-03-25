704273be12c527e8de11d1fb5c00b7ae48ea67dc
Florian Friedrich <ffried@me.com>
2020-03-25 12:33:52 +0100

    Add support for Swift 5.2

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -15,7 +15,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SemVer",
-            dependencies: [],
             exclude: ["Supporting Files"]),
         .testTarget(
             name: "SemVerTests",
