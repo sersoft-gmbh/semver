cb07e1bbd58aabbd68dbe7700e618d11ae2dfdd1
Florian Friedrich <ffried@me.com>
2023-09-20 09:20:51 +0200

    Unconditionally add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -1,7 +1,6 @@
 // swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import Foundation
 import PackageDescription
 import CompilerPluginSupport
 
@@ -29,6 +28,7 @@ let package = Package(
             targets: ["SemVer"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
     ],
     targets: [
@@ -72,7 +72,3 @@ let package = Package(
             swiftSettings: swiftSettings),
     ]
 )
-
-if ProcessInfo.processInfo.environment["ENABLE_DOCC_SUPPORT"] == "1" {
-    package.dependencies.append(.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"))
-}
