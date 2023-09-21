04d34ebbf364667a9caf5efc63e2fe90666c1a7f
Florian Friedrich <ffried@me.com>
2023-09-21 12:35:34 +0200

    Use common actions for tests

--- Tests/SemVerMacrosTests/SemVerMacrosPluginTests.swift
+++ Tests/SemVerMacrosTests/SemVerMacrosPluginTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class SemVerMacrosPluginTests: XCTestCase {
     func testProvidedMacros() {
         let macros = SemVerMacrosPlugin().providingMacros
+        XCTAssertEqual(macros.count, 1)
         XCTAssertTrue(macros.contains { $0 == VersionMacro.self })
     }
 }
